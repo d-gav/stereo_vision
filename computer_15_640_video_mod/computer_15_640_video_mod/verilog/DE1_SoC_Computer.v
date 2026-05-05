748e57ddaa01b5cb163bdcde71f51ec998a08741
Ezra Reiss <er495@cornell.edu>
2026-05-05 16:38:39 -0400

    This contains the SOF of the practically last working SAD

--- computer_15_640_video_mod/computer_15_640_video_mod/verilog/DE1_SoC_Computer.v
+++ computer_15_640_video_mod/computer_15_640_video_mod/verilog/DE1_SoC_Computer.v
@@ -510,7 +510,7 @@ column_prefetch #(
 mem_block_intf #(
 	.FRAME_HEIGHT(FRAME_HEIGHT), .HALF_FRAME_WIDTH(HALF_FRAME_WIDTH),
 	.BLOCK_SIZE(BLOCK_SIZE), .PIXEL_W(PIXEL_W), .MAX_DISP(MAX_DISP),
-	.NUM_SAD_UNITS(8)
+	.NUM_SAD_UNITS(24)
 ) u_mem_block_intf (
 	.clk(CLOCK2_50), .rst(mbi_rst),
 	.go(mbi_go), .stall(mbi_stall),
